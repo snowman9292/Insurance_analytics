d174fcba8e1e47c4d24cb497d590695169bbaa16
Ravi Oruganti <rkorugan@gmail.com>
2025-03-15 22:05:29 +0530

    create customer tab

--- snowflake/1.Landing_zone/landing_claim.sql
+++ snowflake/1.Landing_zone/landing_claim.sql
@@ -2,7 +2,7 @@ CREATE TABLE landing_claim (
     Claim_ID STRING, 
     Policy_ID STRING, 
     Claim_Date DATE, 
-    Claim_Amount NUMBER(12,2), 
+    Claim_Amount NUMBER(12,3), 
     Claim_Status STRING, 
     Load_DTS TIMESTAMP_NTZ 
 ); 
