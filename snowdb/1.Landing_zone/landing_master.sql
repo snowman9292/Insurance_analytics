5cc4cbbb08e60910af54e7e5516a2c8a5d245da5
Ravi Oruganti <rkorugan@gmail.com>
2025-03-17 21:34:11 +0530

    execute landing scripts

--- snowdb/1.Landing_zone/landing_master.sql
+++ snowdb/1.Landing_zone/landing_master.sql
@@ -1,6 +1,6 @@
-!source /home/runner/work/Insurance_analytics/Insurance_analytics/Snowdb/1.Landing_zone/landing_claim.sql
-!source /home/runner/work/Insurance_analytics/Insurance_analytics/Snowdb/1.Landing_zone/landing_customer.sql
-!source /home/runner/work/Insurance_analytics/Insurance_analytics/Snowdb/1.Landing_zone/landing_policy.sql
-!source /home/runner/work/Insurance_analytics/Insurance_analytics/Snowdb/1.Landing_zone/landing_product.sql
-!source /home/runner/work/Insurance_analytics/Insurance_analytics/Snowdb/1.Landing_zone/landing_sales.sql
+!source $GITHUB_WORKSPACE/Snowdb/1.Landing_zone/landing_claim.sql
+!source $GITHUB_WORKSPACE/Snowdb/1.Landing_zone/landing_customer.sql
+!source $GITHUB_WORKSPACE/Snowdb/1.Landing_zone/landing_policy.sql
+!source $GITHUB_WORKSPACE/Snowdb/1.Landing_zone/landing_product.sql
+!source $GITHUB_WORKSPACE/Snowdb/1.Landing_zone/landing_sales.sql
 
