8cfaf51196a7484dce2c312e71524de59a38199c
Ravi Oruganti <rkorugan@gmail.com>
2025-03-17 21:31:07 +0530

    execute landing scripts

--- snowdb/1.Landing_zone/landing_master.sql
+++ snowdb/1.Landing_zone/landing_master.sql
@@ -1,6 +1,6 @@
-!source "/home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/1.Landing_zone/landing_claim.sql"
-!source "/home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/1.Landing_zone/landing_customer.sql"
-!source "/home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/1.Landing_zone/landing_policy.sql"
-!source "/home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/1.Landing_zone/landing_product.sql"
-!source "/home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/1.Landing_zone/landing_sales.sql"
+!source "/home/runner/work/Insurance_analytics/Insurance_analytics/Snowdb/1.Landing_zone/landing_claim.sql"
+!source "/home/runner/work/Insurance_analytics/Insurance_analytics/Snowdb/1.Landing_zone/landing_customer.sql"
+!source "/home/runner/work/Insurance_analytics/Insurance_analytics/Snowdb/1.Landing_zone/landing_policy.sql"
+!source "/home/runner/work/Insurance_analytics/Insurance_analytics/Snowdb/1.Landing_zone/landing_product.sql"
+!source "/home/runner/work/Insurance_analytics/Insurance_analytics/Snowdb/1.Landing_zone/landing_sales.sql"
 
