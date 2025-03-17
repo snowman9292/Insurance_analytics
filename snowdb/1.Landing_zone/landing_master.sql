ebc78a29f521fd67b2d61763817d7d86d7cfc918
Ravi Oruganti <rkorugan@gmail.com>
2025-03-17 22:13:29 +0530

    Execute landing zone scripts

--- snowdb/1.Landing_zone/landing_master.sql
+++ snowdb/1.Landing_zone/landing_master.sql
@@ -1,3 +1,7 @@
+
+USE DATABASE Insurance_analytics;
+USE SCHEMA LANDING_ZONE;
+
 !source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/1.Landing_zone/landing_claim.sql
 !source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/1.Landing_zone/landing_customer.sql
 !source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/1.Landing_zone/landing_policy.sql
