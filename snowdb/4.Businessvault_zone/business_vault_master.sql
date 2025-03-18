ecdea125ee8e977412323b1e28f2d4635c66a7f2
Ravi Oruganti <rkorugan@gmail.com>
2025-03-18 21:38:05 +0530

    Deploy BusinessVault tables

--- snowdb/4.Businessvault_zone/business_vault_master.sql
+++ snowdb/4.Businessvault_zone/business_vault_master.sql
@@ -1,5 +1,4 @@
-!source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/4.Businessvault_zone/hub_claim.sql
-!source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/4.Businessvault_zone/hub_customer.sql
-!source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/4.Businessvault_zone/hub_policy.sql
-!source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/4.Businessvault_zone/hub_product.sql
-!source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/4.Businessvault_zone/hub_sales.sql
+!source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/4.Businessvault_zone/pit_customer_policy.sql
+!source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/4.Businessvault_zone/bridge_active_claims .sql
+!source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/4.Businessvault_zone/agg_policy_sales.sql
+!source /home/runner/work/Insurance_analytics/Insurance_analytics/snowdb/4.Businessvault_zone/agg_claim_summary.sql
