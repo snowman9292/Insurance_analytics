0aebaf69aeb1f9b8a2e8c52bd6a8956734c45a10
Ravi Oruganti <rkorugan@gmail.com>
2025-03-19 23:09:49 +0530

    Load Landing tables

--- ETL/python/load_stage_table.py
+++ ETL/python/load_stage_table.py
@@ -32,7 +32,7 @@ table_file_map = {
 for table, file_pattern in table_file_map.items():
     csv_file = os.path.join(CSV_DIR, f"{table}.csv")
     if os.path.exists(csv_file):
-        put_command = f"PUT file://{csv_file} @{database.schema.SNOWFLAKE_STAGE}"
+        put_command = f"PUT file://{csv_file} @{SNOWFLAKE_STAGE}"
         cursor.execute(put_command)
         print(f"✅ Uploaded {csv_file} to Snowflake Internal Stage.")
     else:
