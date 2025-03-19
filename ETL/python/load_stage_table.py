71642a1393e873b68b6b39c0b68145df5ba4db7a
Ravi Oruganti <rkorugan@gmail.com>
2025-03-19 23:41:21 +0530

    Load Landing tables

--- ETL/python/load_stage_table.py
+++ ETL/python/load_stage_table.py
@@ -49,7 +49,7 @@ for table, file_pattern in table_file_map.items():
     FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1)
     PATTERN = '{file_pattern}';
     """
-    cur.execute(sql)
+    cursor.execute(sql)
     print(f"✅ Data loaded into {table}")
 
 cur.close()
