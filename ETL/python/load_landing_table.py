17262cc2da6b1dbf44eb8cbf804592fb262f5d62
Ravi <rkorugan@gmail.com>
2025-05-26 18:03:39 +0000

    rdeploy

--- ETL/python/load_landing_table.py
+++ ETL/python/load_landing_table.py
@@ -6,12 +6,12 @@ load_dotenv()
 
 # Snowflake connection details from environment variables
 conn = snowflake.connector.connect(
-    user=os.getenv["SNOWFLAKE_USER"],
-    password=os.getenv["SNOWFLAKE_PASSWORD"],
-    account=os.getenv["SNOWFLAKE_ACCOUNT"],
-    warehouse=os.getenv["SNOWFLAKE_WAREHOUSE"],
-    database=os.getenv["SNOWFLAKE_DATABASE"],
-    schema=os.getenv["LANDING_SCHEMA"]
+    user=os.getenv("SNOWFLAKE_USER"),
+    password=os.getenv("SNOWFLAKE_PASSWORD"),
+    account=os.getenv("SNOWFLAKE_ACCOUNT"),
+    warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+    database=os.getenv("SNOWFLAKE_DATABASE"),
+    schema=os.getenv("LANDING_SCHEMA")
 )
 print(user)
 print(account)
